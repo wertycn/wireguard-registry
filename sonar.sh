1ba3012271c0a8814bcc80d62b1b28bae5ada063
debug.icu <hajnxg@126.com>
2024-02-21 23:10:40 +0800

    feat 调整编译配置，新增公共Docker仓库推送配置

--- sonar.sh
+++ sonar.sh
@@ -4,4 +4,5 @@ mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.9.1.2184:sonar \
  -Dsonar.token=${SONAR_TOKEN} \
  -Dsonar.host.url=https://sonarcloud.io \
  -Dsonar.organization=wertycn \
- -Dsonar.projectKey=wertycn_wireguard-registry
+ -Dsonar.projectKey=wertycn_wireguard-registry \
+ -DskipTests
